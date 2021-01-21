5e5e7c3707557f4566cf8527f474b677697e3066
Demetrio Alvarez <daiglesias3@esei.uvigo.es>
2021-01-21 21:59:12 +0100

    Created SliceMasters page

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -76,6 +76,7 @@ async function fetchBeersAndTurnIntoNodes({
     });
   }
 }
+
 async function turnSlicemastersIntoPages({ graphql, actions }) {
   const { data } = await graphql(`
     query {
@@ -92,8 +93,20 @@ async function turnSlicemastersIntoPages({ graphql, actions }) {
     }
   `);
 
+  data.slicemasters.nodes.forEach((slicemaster) => {
+    actions.createPage({
+      component: path.resolve('./src/templates/Slicemaster.js'),
+      path: `/slicemaster/${slicemaster.slug.current}`,
+      context: {
+        name: slicemaster.person,
+        slug: slicemaster.slug.current,
+      },
+    });
+  });
+
   const pageSize = parseInt(process.env.GATSBY_PAGE_SIZE);
   const pageCount = Math.ceil(data.slicemasters.totalCount / pageSize);
+
   Array.from({ length: pageCount }).forEach((_, i) => {
     actions.createPage({
       path: `/slicemasters/${i + 1}`,
